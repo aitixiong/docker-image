e18c5abd49291a0b96f3539d94a74c875f0c2ecb
liuyunwuhen <liuyunwuhen@gmail.com>
2023-08-10 14:45:07 +0800

    Add files via upload

---
+++ loganalyzer/build.sh
@@ -0,0 +1,4 @@
+wget https://download.adiscon.com/loganalyzer/loganalyzer-4.1.13.tar.gz
+tar -zxvf loganalyzer-4.1.13.tar.gz
+docker buildx build -t liuyunwuhen/loganalyzer:alpine --platform=linux/arm64,linux/amd64 . --push
+
---
+++ rsyslog/build.sh
@@ -0,0 +1,2 @@
+docker buildx build -t liuyunwuhen/rsyslog:alpine --platform=linux/arm64,linux/amd64 . --push
+
