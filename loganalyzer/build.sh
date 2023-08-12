990f072d875f25069fae41ad3e7a3e77a177dabf
liuyunwuhen <liuyunwuhen@gmail.com>
2023-08-12 10:29:41 +0800

    Update build.sh

--- loganalyzer/build.sh
+++ loganalyzer/build.sh
@@ -1,4 +1,3 @@
-wget https://download.adiscon.com/loganalyzer/loganalyzer-4.1.13.tar.gz
-tar -zxvf loganalyzer-4.1.13.tar.gz
+# wget https://download.adiscon.com/loganalyzer/loganalyzer-4.1.13.tar.gz
+tar -zxvf loganalyzer-4.1.13-src.tar.gz
 docker buildx build -t liuyunwuhen/loganalyzer:alpine --platform=linux/arm64,linux/amd64 . --push
-
